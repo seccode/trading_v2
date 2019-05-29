afa070671847c45107b7c8db9dc42e7b625fd3a1
Brendan Hogan Rappazzo <brendanrappazzo@Brendans-MacBook-Pro.local>
2019-05-28 20:43:56 -0400

    made it fast

--- pattern.py
+++ pattern.py
@@ -6,20 +6,54 @@ from sklearn.metrics.pairwise import cosine_similarity, euclidean_distances
 from data_loader import data_loader
 from scipy.stats import levene
 import numpy as np
+from numba import jit
+import time
 
 
-def reverse_price_scale(a1):
-    vals = [0]
-    for x in range(1,len(a1)):
-        vals.append(np.mean(a1[len(a1)-1-x]) - np.mean(a1[len(a1)-1]))
-    return np.array(vals[::-1])
 
-def price_scale(a1):
-    vals = [0]
-    for x in range(1,len(a1)):
-        vals.append(np.mean(a1[x]) - np.mean(a1[0]))
-    return np.array(vals)
+@jit(nopython=True,parallel=True)
+def fast_reverse_price_scale(a1):
+    # Assume a1 is np vector (n,)
+    vals = np.zeros((a1.shape[0]))
+    vals[0] = 0
+    for i in range(1,a1.shape[0]):
+        vals[i] = a1[-i] - a1[-1]
+    return vals[::-1]
 
+
+@jit(nopython=True,parallel=True)
+def fast_price_scale(a1):
+    # Assume a1 is np vector (n,)
+    vals = np.zeros((a1.shape[0]))
+    vals[0] = 0
+    for i in range(1,a1.shape[0]):
+        vals[i] = a1[i] - a1[0]
+    return vals
+
+
+@jit(nopython=True,parallel=True)
+def fast_euclidean_distance(x,y):
+    distance_vect = np.zeros((x.shape[0]))
+    for i in range(x.shape[0]):
+        rolling_distance = 0
+        for j in range(x.shape[1]):
+            difference = x[i,j]-y[0,j]
+            rolling_distance += difference*difference
+        distance_vect[i] = np.sqrt(rolling_distance)
+    return distance_vect # nx1
+
+@jit(nopython=True,parallel=True)
+def fast_make_patterns(train_data,look_back,look_forward):
+    i = look_back
+    patterns = np.zeros((train_data.shape[0] - look_forward-look_back,look_back+1))
+    outcomes = np.zeros((train_data.shape[0] - look_forward-look_back,look_forward))
+    volumes = np.zeros((train_data.shape[0] - look_forward-look_back,look_back+1))
+    while i < train_data.shape[0] - look_forward:
+        patterns[i-look_back,:] = fast_reverse_price_scale(train_data[i-look_back:i+1,3])
+        outcomes[i-look_back,:] = fast_price_scale(train_data[i:i+look_forward,3])
+        volumes[i-look_back,:] = train_data[i-look_back:i+1,7]
+        i = i + 1
+    return patterns,outcomes,volumes
 
 class Trader():
 
@@ -29,18 +63,10 @@ class Trader():
 
     def make_patterns(self):
         # Loop through training data and create stored patterns
-        patterns = []
-        outcomes = []
-        volumes = []
-        i = self.look_back
-        while i < self.train_data.shape[0] - self.look_forward:
-            patterns.append(reverse_price_scale(self.train_data[i-self.look_back:i+1,3]))
-            outcomes.append(price_scale(self.train_data[i:i+self.look_forward,3]))
-            volumes.append(self.train_data[i-self.look_back:i+1,7])
-            i += 1
-        self.patterns = np.array(patterns)
-        self.outcomes = np.array(outcomes)
-        self.volumes = np.array(volumes)
+        start_time = time.time()
+        self.patterns,self.outcomes,self.volumes = fast_make_patterns(self.train_data,self.look_back,self.look_forward)
+        print('Data took {}'.format(time.time()-start_time))
+
 
     def long(self,max_time,take,stop):
         # Long position
@@ -95,14 +121,16 @@ class Trader():
             #     print("On day {}".format(current_day))
             # Get current pattern and outcome
             spread = self.data[self.index,6] - self.data[self.index,3]
-            current_pattern = reverse_price_scale(self.data[self.index-self.look_back:self.index+1,3])
-            current_outcome = price_scale(self.data[self.index:self.index+self.look_forward,3])
+            current_pattern = fast_reverse_price_scale(self.data[self.index-self.look_back:self.index+1,3])
+            current_outcome = fast_price_scale(self.data[self.index:self.index+self.look_forward,3])
             current_volume = self.data[self.index-self.look_back:self.index+1,7]
 
             # Compare current pattern to stored patterns
             # cs = cosine_similarity(self.patterns,current_pattern.reshape(1,-1))
-            cs = euclidean_distances(self.patterns,current_pattern.reshape(1,-1))
-
+            start = time.time()
+            #cs = euclidean_distances(self.patterns,current_pattern.reshape(1,-1))
+            cs = fast_euclidean_distance(self.patterns,current_pattern.reshape(1,-1))
+            #cs = cs.reshape(-1,1)
             # Find indices where pattern similarity is high
             inds = np.where(cs < np.sort(cs.flatten())[40])[0]
             if inds.shape[0] < 4:
@@ -148,16 +176,16 @@ class Trader():
         return round(100 * (self.money[-1] / self.money[0] - 1),2)
 
 
-train_data, _ = data_loader('EUR_USD','01/02/19','2100','02/25/19','2100')
-test_data, _ = data_loader('EUR_USD','02/25/19','2100','03/11/19','2100')
+train_data, _ = data_loader('EUR_USD','01/02/19','2100','01/20/19','2100')
+test_data, _ = data_loader('EUR_USD','02/25/19','2100','02/26/19','2100')
 m = Trader(train_data,test_data)
 
-bounds = np.array([ [30,120], [10,60], [.0001,.0015], [.01,.25], [1,15] ])
-best_params = bayesian_optimisation(200,m.trade,bounds)
+#bounds = np.array([ [30,120], [10,60], [.0001,.0015], [.01,.25], [1,15] ])
+#best_params = bayesian_optimisation(200,m.trade,bounds)
 
-# parameters = [71, 15, 0.00036, 0.07, 8]
-# ror = m.trade(parameters)
-# print("Rate of Return: {}%".format(ror))
+parameters = [71, 15, 0.00036, 0.07, 8]
+ror = m.trade(parameters)
+print("Rate of Return: {}%".format(ror))
 
 
 
